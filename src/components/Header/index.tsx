6d01225d9d50911b63457ba83bd25a8a3841c7c8
DucHuySET <huy.nd203450@sis.hust.edu.vn>
2023-09-10 16:44:14 +0700

    giới thiệu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -40,7 +40,7 @@ const Header = ({ t }: any) => {
     return (
       <>
         <CustomNavLinkSmall onClick={() => scrollTo("about")}>
-          <Span>{t("About")}</Span>
+          <Span>{t("Giới thiệu")}</Span>
         </CustomNavLinkSmall>
         <CustomNavLinkSmall onClick={() => scrollTo("mission")}>
           <Span>{t("Mission")}</Span>
